32da8cf3501af161696382d52bf401a8d4f72ac5
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-04-28 18:36:26 +0300

    Step5: Pull up equals and hashCode from Dollar to Money

--- src/main/java/com/fmdev/tdd/multimoney/Dollar.java
+++ src/main/java/com/fmdev/tdd/multimoney/Dollar.java
@@ -6,21 +6,7 @@ public class Dollar extends Money {
     }
 
     public Dollar times(int multiplier) {
-        return new Dollar(amount * multiplier);
+        return new Dollar(hashCode() * multiplier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Money money = (Money) o;
-
-        return amount == money.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return amount;
-    }
 }
--- src/main/java/com/fmdev/tdd/multimoney/Money.java
+++ src/main/java/com/fmdev/tdd/multimoney/Money.java
@@ -2,4 +2,19 @@ package com.fmdev.tdd.multimoney;
 
 public class Money {
     protected int amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Money money = (Money) o;
+
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return amount;
+    }
 }
