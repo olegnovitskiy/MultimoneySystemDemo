253f5994b9fbb2882154196e734295abf2d65aee
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-04-28 21:58:03 +0300

    Step14: Add parameter in Bank

--- src/main/java/com/fmdev/tdd/multimoney/Bank.java
+++ src/main/java/com/fmdev/tdd/multimoney/Bank.java
@@ -2,7 +2,7 @@ package com.fmdev.tdd.multimoney;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     public void addRate(String chf, String usd, int i) {
