b7e3fb526feaf3f4a7d89e9141920c1235c24933
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-04-28 18:44:02 +0300

    Step5: Done!

--- src/main/java/com/fmdev/tdd/multimoney/Franc.java
+++ src/main/java/com/fmdev/tdd/multimoney/Franc.java
@@ -8,20 +8,4 @@ public class Franc extends Money {
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Money money = (Money) o;
-
-        return amount == money.amount;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return amount;
-    }
 }
